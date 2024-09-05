75223ae756efd551689b525c9088bc3429ddf5b1
sunnyksharma <sunnysharma5959@gmail.com>
2024-09-05 23:42:35 +0530

    init commit

---
+++ src/main/java/com/tryouts/config/server/ConfigServerTryoutsApplication.java
@@ -0,0 +1,14 @@
+package com.tryouts.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ConfigServerTryoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerTryoutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tryouts/config/server/ConfigServerTryoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryouts.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerTryoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
